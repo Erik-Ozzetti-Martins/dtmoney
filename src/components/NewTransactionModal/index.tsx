37f95a349130c2684a54c6de90429c00efeab111
erik ozzetti martins <erik.ozzetti.martins@gmail.com>
2022-03-27 01:01:43 -0300

    envio de dados

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,6 +4,7 @@ import { Container, RadioBox, TransactionTypeContainer } from './styles'
 import incomeImg from '../../assets/income.svg'
 import outComeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg'
+import { api } from '../../services/api'
 
 interface NewTransactionModalProps{
   isOpen:boolean
@@ -19,13 +20,13 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
 
   function handleCreateNewTransaction(event:FormEvent) {
     event.preventDefault()
-    console.log({
+    const data = {
       title,
       value,
       category,
       type
-    })
-
+    }
+    api.post('/transactions', data)
   }
 
   return(
