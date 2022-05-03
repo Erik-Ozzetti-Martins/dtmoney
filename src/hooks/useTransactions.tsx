7b9d3e2c16863b8cbdb6c8cd12151a4eced57fd4
erik ozzetti martins <erik.ozzetti.martins@gmail.com>
2022-05-03 19:53:13 -0300

    use

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,20 +1,27 @@
-import React, { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import React, {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { api } from "../services/api";
 
-const TransactionsContext = createContext<TransactionsContextData>({} as TransactionsContextData);
-
+const TransactionsContext = createContext<TransactionsContextData>(
+  {} as TransactionsContextData
+);
 
 interface Transaction {
-  id:number;
-  title:string;
-  amount:number;
-  category:string;
-  type:string;
-  createdAt: string;
+  id: string;
+  title: string;
+  amount: number;
+  category: string;
+  type: string;
+  created_at: string;
 }
 
 interface TransactionsProviderProps {
-  children : ReactNode
+  children: ReactNode;
 }
 /*
 interface TransactionInput{
@@ -24,38 +31,49 @@ interface TransactionInput{
   type:string;
 }
 */
-type TransactionInput= Omit<Transaction,'id' | 'createdAt'>
+type TransactionInput = Omit<Transaction, "id" | "created_at">;
 
 interface TransactionsContextData {
-  transactions:Transaction[];
-  createTransaction:(transaction: TransactionInput ) => Promise<void>;
+  transactions: Transaction[];
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
-
-export function TransactionsProvider({ children }:TransactionsProviderProps){
-
-  const [transactions, setTransactions] = useState< Transaction[]>([]);
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
 
   useEffect(() => {
-    api.get('/transactions')
-      .then(response => setTransactions(response.data.transactions))
-    },[])
+    const token = window.localStorage.getItem("token");
+    if (token) {
+      api
+        .get("/ledger", { headers: { Authorization: `Bearer ${token}` } })
+        .then((response) => setTransactions(response.data.ledger));
+    }
+  }, []);
 
-  async function createTransaction(transactionInput:TransactionInput){
-    const response = await api.post('/transactions', {...transactionInput, createdAt: new Date()})
-    const  { transaction } = response.data
-
-    setTransactions([...transactions,transaction])
+  async function createTransaction(transactionInput: TransactionInput) {
+    const token = window.localStorage.getItem("token");
+    if (token) {
+      const response = await api.post(
+        "/ledger",
+        {
+          ...transactionInput,
+        },
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+      console.log(response);
+      const { ledger } = response.data;
+      setTransactions([...transactions, ledger]);
+    }
   }
 
   return (
-    <TransactionsContext.Provider value={{transactions,createTransaction}}>
+    <TransactionsContext.Provider value={{ transactions, createTransaction }}>
       {children}
     </TransactionsContext.Provider>
-  )
+  );
 }
 
-export function useTransactions(){
-  const context = useContext(TransactionsContext)
-  return context
+export function useTransactions() {
+  const context = useContext(TransactionsContext);
+  return context;
 }
