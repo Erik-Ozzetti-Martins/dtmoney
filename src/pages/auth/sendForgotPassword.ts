d1df5b479b760dde9af3b127ea3367ff48021375
erik ozzetti martins <erik.ozzetti.martins@gmail.com>
2022-06-20 03:04:46 -0300

    responsividade

--- src/pages/auth/sendForgotPassword.ts
+++ src/pages/auth/sendForgotPassword.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import media from "styled-media-query";
 
 export const Container = styled.div`
   width: 100vw;
@@ -6,7 +7,7 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  div {
+  & > div {
     width: 30rem;
     height: 30rem;
     margin-right: 2rem;
@@ -15,6 +16,13 @@ export const Container = styled.div`
       height: 100%;
       mix-blend-mode: multiply;
     }
+    ${media.lessThan("small")`
+      display: flex;
+      justify-content:center;
+      height: auto;
+      margin-right: initial;
+      padding: 0 2rem;
+    `}
   }
   form {
     width: 30rem;
@@ -44,7 +52,7 @@ export const Container = styled.div`
       height: 4rem;
       background: var(--green);
       color: #fff;
-      border-radius: 0.25;
+      border-radius: 0.25rem;
       border: 0;
       font-size: 1rem;
       margin-top: 1.5rem;
@@ -61,5 +69,12 @@ export const Container = styled.div`
       margin-top: 0.5;
       color: red;
     }
+    ${media.lessThan("small")`
+    width: 100%;
+  `}
   }
+  ${media.lessThan("medium")`
+    flex-direction: column;
+    padding: 0 2rem;
+  `}
 `;
